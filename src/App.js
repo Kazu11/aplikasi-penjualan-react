cb01ab6442e8eea71cde09ae53aab745ad2b95b3
Kazu11 <43257615+Kazu11@users.noreply.github.com>
2021-02-21 02:43:28 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
 import {BrowserRouter as Router, Route,  Switch} from 'react-router-dom';
-import Login from './pages/login';
-import PasswordForget from './pages/password-forget';
-import Private from './pages/private';
-
 
 //komponen Registrasi
 import Registrasi from "./pages/registrasi";
 import Unknown from './pages/unknown';
-
+import Login from './pages/login';
+import PasswordForget from './pages/password-forget';
+import Private from './pages/private';
+import PrivateRoute from './component/PrivateRoute';
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/pengaturan" component={Private}/>
+        <PrivateRoute path="/" exact component={Private}/>
+        <PrivateRoute path="/pengaturan" component={Private}/>
+        <PrivateRoute path="/produk" component={Private}/>
+        <PrivateRoute path="/transaksi" component={Private}/>
         <Route path="/registrasi" component={Registrasi}/>
         <Route path="/login" component={Login}/>
         <Route path="/password-forget" component={PasswordForget}/>
